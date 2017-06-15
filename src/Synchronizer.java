dd90581aa2267602a005fbe77faf54a5214c0257
MaciejSzymczak <soft@home.pl>
2017-06-15 06:16:07 +0200

    Table of contents - better visual design

--- src/Synchronizer.java
+++ src/Synchronizer.java
@@ -230,7 +230,7 @@ import java.util.zip.CRC32;
 			
 			ReadDirectory icsProcessed = new ReadDirectory(folderName+  "\\processed");				
 			icsProcessed.readIcsFilesFromFolder( new File(folderName+  "\\processed") );			
-			tableofContents(icsProcessed, folderName+"\\ListOfCalendars.html");
+			tableofContents(icsProcessed, folderName+"\\ListOfCalendars.js");
 			
 	    //} catch (IOException e) {
 	    //  System.err.println(e.getMessage());
@@ -351,25 +351,8 @@ import java.util.zip.CRC32;
 		  
 		  System.out.println( String.format("Creating file %s...",fileName));		  
 
-		  fw.write("<!DOCTYPE html>\n<html lang=\"pl\">\n<HEAD><META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=utf-8\"></HEAD>\n<body>\n");
-		  
-		  //like
-		  fw.write("<div id=\"fb-root\"></div>\n");
-		  fw.write("<script>// <![CDATA[\n");
-		  fw.write("(function(d, s, id) {\n");
-		  fw.write("  var js, fjs = d.getElementsByTagName(s)[0];\n");
-		  fw.write("  if (d.getElementById(id)) return;\n");
-		  fw.write("  js = d.createElement(s); js.id = id;\n");
-		  fw.write("  js.src = \"//connect.facebook.net/pl_PL/all.js#xfbml=1&version=v2.5\";\n");
-		  fw.write("  fjs.parentNode.insertBefore(js, fjs);\n");
-		  fw.write("}(document, 'script', 'facebook-jssdk'));\n");
-		  fw.write("// ]]></script>\n");
-		  fw.write("<div class=\"fb-like\" data-href=\"https://www.facebook.com/Plansoft.org\" data-layout=\"standard\" data-action=\"like\" data-show-faces=\"true\" data-share=\"true\"></div>\n");
-		  		  
-		  fw.write("<h1 style=\"font-variant: small-caps; text-align: center; color: white; background-color: black; \">Lista kalendarzy do zaimportowania do Twojego kalendarza Google</h1>"); 
-		  fw.write(String.format("<a href=\"%s\">%s</a><br/><br/>\n", "http://plansoft.org/Sciaga.JakWyswietlicKalendarz.pdf", "W jaki sposób mogę pobrać kalendarz?"));
-		  
-		  fw.write("<table  border=\"1\" width=\"80%\" style=\"font-variant: small-caps; border: 1px dashed silver\">\n");
+		  String separator = "";
+		  fw.write("var data = [\n");
 		  for(Object entry: ics.calendars.keySet()) {
 				String calName = (String)entry;
 				String calendarId= "***Calendar not found***";
@@ -378,14 +361,10 @@ import java.util.zip.CRC32;
 				} catch (Exception e) {}
 				if ((calendarId+"").length()==0) 
 					calendarId= "***Calendar not found***";
-				fw.write("<tr><td>");
-				fw.write(String.format("<a href=\"https://calendar.google.com/calendar/ical/%s/public/basic.ics\">%s</a>", calendarId, calName));
-				fw.write("</td><td>");
-				fw.write(String.format("https://calendar.google.com/calendar/ical/%s/public/basic.ics", calendarId));
-				fw.write("</td></tr>");
+				fw.write(String.format("%s{ calendarName: '%s', link: 'https://calendar.google.com/calendar/ical/%s/public/basic.ics' }", separator, calendarId, calName));
+				separator=",";
 				}		  
-		  fw.write("</table>\n");
-		  fw.write("</body>\n</html>");
+		  fw.write("];");
 		  fw.close();		  
 	  }
 	  
