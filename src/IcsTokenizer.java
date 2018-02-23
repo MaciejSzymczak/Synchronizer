0bebe24db49d506d804d5283222a527ddeb95532
MaciejSzymczak <soft@home.pl>
2018-02-23 06:21:15 +0100

    Ignoring empty lines in .ics content

--- src/IcsTokenizer.java
+++ src/IcsTokenizer.java
@@ -3,6 +3,7 @@ public class IcsTokenizer {
 	String key;
 	String value;
 	IcsTokenizer (String s) {
+		if (s==null || s.length()==0) {s=":";} //ignore empty lines
 		int i = s.indexOf(":");
 		key = s.substring(0, i);
 		value = s.substring(i+1, s.length());
